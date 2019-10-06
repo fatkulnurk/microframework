97c6d9bd29bc4a82f7e86c3fd8d2c1236de93be0
fatkulnurk <fatkulnurk@gmail.com>
2019-10-06 23:15:45 +0700

    add a routing

---
+++ src/route.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Route;
+
+use Mifa\Routing\RouteCollector;
+
+return function (RouteCollector $r){
+    $r->addRoute('GET', '/', function (){
+
+    });
+
+    $r->addGroup('/dashboard', function (){
+
+    });
+
+};
