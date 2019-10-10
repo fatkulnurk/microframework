8ba83c91f9b5cc36c116875370b774628e314a72
fatkulnurk <fatkulnurk@gmail.com>
2019-10-10 21:57:30 +0700

    remove group

--- src/route.php
+++ src/route.php
@@ -4,9 +4,4 @@ use Mifa\Routing\RouteCollector;
 return function (RouteCollector $r) {
     $r->addRoute('GET', '/', function (){
     });
-
-    $r->addGroup('/dashboard', function (){
-
-    });
-
 };
