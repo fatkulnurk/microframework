748160feedf65037a751e561d69751501c495e9b
fatkulnurk <fatkulnurk@gmail.com>
2020-01-08 23:59:30 +0700

    fixing error in message traits

--- system/Http/MessageTrait.php
+++ system/Http/MessageTrait.php
@@ -1,28 +1,26 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Fatkulnurk\Microframework\Http\Message;
 
-use InvalidArgumentException;
-use phpDocumentor\Reflection\Types\This;
-use Psr\Http\Message\MessageInterface;
+//use Nyholm\Psr7\Stream;
 use Psr\Http\Message\StreamInterface;
-use function is_array;
-use function is_numeric;
-use function is_string;
-use function preg_match;
-use function strtolower;
-use function trim;
 
 //class MessageTrait implements MessageInterface
 trait MessageTrait
 {
+
+    /** @var array Map of all registered headers, as original name => array of values */
     private $headers = [];
 
+    /** @var array Map of lowercase header name => original name at registration */
     private $headerNames = [];
 
+    /** @var string */
     private $protocol = '1.1';
 
+    /** @var StreamInterface|null */
     private $stream;
 
     public function getProtocolVersion(): string
@@ -49,12 +47,12 @@ trait MessageTrait
 
     public function hasHeader($header): bool
     {
-        return isset($this->headerNames[strtolower($header)]);
+        return isset($this->headerNames[\strtolower($header)]);
     }
 
     public function getHeader($header): array
     {
-        $header = strtolower($header);
+        $header = \strtolower($header);
         if (!isset($this->headerNames[$header])) {
             return [];
         }
@@ -72,95 +70,70 @@ trait MessageTrait
     public function withHeader($header, $value): self
     {
         $value = $this->validateAndTrimHeader($header, $value);
-        $normalized = strtolower($header);
+        $normalized = \strtolower($header);
 
-        $newThis = clone $this;
+        $new = clone $this;
         if (isset($new->headerNames[$normalized])) {
-            unset($newThis->headers[$newThis->headerNames[$normalized]]);
+            unset($new->headers[$new->headerNames[$normalized]]);
         }
-        $newThis->headerNames[$normalized] = $header;
-        $newThis->headers[$header] = $value;
+        $new->headerNames[$normalized] = $header;
+        $new->headers[$header] = $value;
 
-        return $newThis;
+        return $new;
     }
 
-
-    /**
-     * Method untuk menambahkan custom header
-     * @param string $header
-     * @param string|string[] $value
-     * @return $this
-     */
     public function withAddedHeader($header, $value): self
     {
-        if (!is_string($header) || $header === '') {
-            throw new InvalidArgumentException('Header name must be an RFC 7230 compatible string.');
+        if (!\is_string($header) || '' === $header) {
+            throw new \InvalidArgumentException('Header name must be an RFC 7230 compatible string.');
         }
 
-        $newThis = clone $this;
-        $newThis->setHeaders([$header => $value]);
+        $new = clone $this;
+        $new->setHeaders([$header => $value]);
 
-        return $newThis;
+        return $new;
     }
 
     public function withoutHeader($header): self
     {
-        $normalized = strtolower($header);
+        $normalized = \strtolower($header);
         if (!isset($this->headerNames[$normalized])) {
             return $this;
         }
 
         $header = $this->headerNames[$normalized];
-        $newThis = clone $this;
-        unset($newThis->headers[$header], $newThis->headerNames[$normalized]);
+        $new = clone $this;
+        unset($new->headers[$header], $new->headerNames[$normalized]);
 
-        return $newThis;
+        return $new;
     }
 
     public function getBody(): StreamInterface
     {
-        if ($this->stream) {
+        if (null === $this->stream) {
             $this->stream = Stream::create('');
         }
 
         return $this->stream;
     }
 
-    /**
-     * Return an instance with the specified message body.
-     *
-     * The body MUST be a StreamInterface object.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return a new instance that has the
-     * new body stream.
-     *
-     * @param StreamInterface $body Body.
-     * @return static
-     * @throws InvalidArgumentException When the body is not valid.
-     */
     public function withBody(StreamInterface $body): self
     {
         if ($body === $this->stream) {
             return $this;
         }
 
-        $newThis = clone $this;
-        $newThis->stream = $body;
+        $new = clone $this;
+        $new->stream = $body;
 
-        return $newThis;
+        return $new;
     }
 
-    /**
-     * @param array $headers
-     */
-    public function setHeaders(array $headers): void
+    private function setHeaders(array $headers): void
     {
-//        $this->headers = $headers;
-
         foreach ($headers as $header => $value) {
             $value = $this->validateAndTrimHeader($header, $value);
-            $normalized = strtolower($header);
+            $normalized = \strtolower($header);
             if (isset($this->headerNames[$normalized])) {
                 $header = $this->headerNames[$normalized];
                 $this->headers[$header] = \array_merge($this->headers[$header], $value);
@@ -171,42 +144,51 @@ trait MessageTrait
         }
     }
 
+    /**
+     * Make sure the header complies with RFC 7230.
+     *
+     * Header names must be a non-empty string consisting of token characters.
+     *
+     * Header values must be strings consisting of visible characters with all optional
+     * leading and trailing whitespace stripped. This method will always strip such
+     * optional whitespace. Note that the method does not allow folding whitespace within
+     * the values as this was deprecated for almost all instances by the RFC.
+     *
+     * header-field = field-name ":" OWS field-value OWS
+     * field-name   = 1*( "!" / "#" / "$" / "%" / "&" / "'" / "*" / "+" / "-" / "." / "^"
+     *              / "_" / "`" / "|" / "~" / %x30-39 / ( %x41-5A / %x61-7A ) )
+     * OWS          = *( SP / HTAB )
+     * field-value  = *( ( %x21-7E / %x80-FF ) [ 1*( SP / HTAB ) ( %x21-7E / %x80-FF ) ] )
+     *
+     * @see https://tools.ietf.org/html/rfc7230#section-3.2.4
+     */
     private function validateAndTrimHeader($header, $values): array
     {
-        $compatibleStringMessage = 'Header name must be an RFC 7230 compatible string.';
-        $compatibleStringPattern = "@^[!#$%&'*+.^_`|~0-9A-Za-z-]+$@";
-        if (!is_string($header) || 1 !== preg_match($compatibleStringPattern, $header)) {
-            throw new InvalidArgumentException($compatibleStringMessage);
+        if (!\is_string($header) || 1 !== \preg_match("@^[!#$%&'*+.^_`|~0-9A-Za-z-]+$@", $header)) {
+            throw new \InvalidArgumentException('Header name must be an RFC 7230 compatible string.');
         }
 
-        $pattern = "@^[ \t\x21-\x7E\x80-\xFF]*$@";
-        if (!is_array($values)) {
-            // ini yang sederhana, hanya satu nilai
-            if ((!is_numeric($values) && !is_string($values)) ||
-                1 !== preg_match($pattern, (string) $values)) {
-                throw new InvalidArgumentException($compatibleStringMessage);
+        if (!\is_array($values)) {
+            // This is simple, just one value.
+            if ((!\is_numeric($values) && !\is_string($values)) || 1 !== \preg_match("@^[ \t\x21-\x7E\x80-\xFF]*$@", (string) $values)) {
+                throw new \InvalidArgumentException('Header values must be RFC 7230 compatible strings.');
             }
 
-            return [
-                trim((string) $values, " \t")
-            ];
+            return [\trim((string) $values, " \t")];
         }
 
-        $messageMustStringOrArray = 'Header values must be a string or an array of strings, empty array given.';
         if (empty($values)) {
-            throw new InvalidArgumentException($messageMustStringOrArray);
+            throw new \InvalidArgumentException('Header values must be a string or an array of strings, empty array given.');
         }
 
-        // Assert array yang tidak kosong
+        // Assert Non empty array
         $returnValues = [];
-        $newPattern = "@^[ \t\x21-\x7E\x80-\xFF]*$@";
         foreach ($values as $v) {
-            if ((!\is_numeric($v) && !is_string($v)) ||
-                1 !== preg_match($newPattern, (string) $v)) {
-                throw new InvalidArgumentException($compatibleStringMessage);
+            if ((!\is_numeric($v) && !\is_string($v)) || 1 !== \preg_match("@^[ \t\x21-\x7E\x80-\xFF]*$@", (string) $v)) {
+                throw new \InvalidArgumentException('Header values must be RFC 7230 compatible strings.');
             }
 
-            $returnValues[] = trim((string) $v, " \t");
+            $returnValues[] = \trim((string) $v, " \t");
         }
 
         return $returnValues;
