74c2ef3e9f34af58b4848130270a57f5636524b0
fatkulnurk <fatkulnurk@gmail.com>
2020-01-08 23:58:44 +0700

    fixing in response and make force download method

--- system/Http/Response.php
+++ system/Http/Response.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Fatkulnurk\Microframework\Http\Message;
 
+use Fatkulnurk\Microframework\App;
 use Fatkulnurk\Microframework\Core\Singleton;
 use Fatkulnurk\Microframework\Http\Data\Document\Document;
 use Fatkulnurk\Microframework\Http\Data\Document\DocumentFactory;
@@ -11,14 +12,15 @@ use Fatkulnurk\Microframework\Http\Data\Document\XmlFactory;
 use Fatkulnurk\Microframework\Http\Data\View\Page;
 use Fatkulnurk\Microframework\Http\Data\View\TemplateFactory;
 use Fatkulnurk\Microframework\Http\Data\View\TwigTemplateFactory;
-use Psr\Http\Message\{ResponseInterface, StreamInterface};
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 use function is_int;
 use function is_string;
 
 final class Response implements ResponseInterface
 {
 //    use MessageTrait;
-
+//    use MessageTrait;
     use \Nyholm\Psr7\MessageTrait;
     use Singleton;
 
@@ -164,24 +166,24 @@ final class Response implements ResponseInterface
      * RESPONSE LANGSUNG
      * */
 
-    public function withView(string $path, array $data = [], TemplateFactory $templateFactory = null): ResponseInterface
+    public function withView(string $path, array $data = [], TemplateFactory $templateFactory = null, int $status = 200): ResponseInterface
     {
-        var_dump($templateFactory);
+        // var_dump($templateFactory);
         $view = new Page($path, $data);
         if ($templateFactory == null) {
             $resultHTML = $view->render(new TwigTemplateFactory());
         } else {
-            var_dump($templateFactory);
+            // var_dump($templateFactory);
             if ($templateFactory instanceof TemplateFactory) {
                 $resultHTML = $view->render($templateFactory);
+            } else {
+                throw new \Exception('TemplateFactory must be instance of Document Factory');
             }
-
-            throw new \Exception('TemplateFactory must be instance of Document Factory');
         }
 
-        $responseBody = \Nyholm\Psr7\Stream::create($resultHTML);
-//        $responseBody = Stream::create($resultHTML);
-        $response = $this->make(200)
+//        $responseBody = \Nyholm\Psr7\Stream::create($resultHTML);
+        $responseBody = Stream::create($resultHTML);
+        $response = $this->make($status)
             ->withBody($responseBody)
             ->withHeader('Content-Type', 'text/html');
 
@@ -192,39 +194,139 @@ final class Response implements ResponseInterface
         return $response;
     }
 
-    public function withJson($data)
+    public function withJson($data, $status = 200)
     {
         $json = new Document($data);
         $resultJson = $json->result(new JsonFactory);
         $responseBody = \Nyholm\Psr7\Stream::create($resultJson);
-        $response = $this->make(200)
+        $response = $this->make($status)
             ->withBody($responseBody)
             ->withHeader('Content-Type', 'application/json');
 
         return $response;
     }
 
-    public function withXml($data)
+    public function withXml($data, $status = 200)
     {
         $xml = new Document($data);
         $resultXml = $xml->result(new XmlFactory());
 
         $responseBody = Stream::create($resultXml);
-        $response = $this->make(200)
+        $response = $this->make($status)
             ->withBody($responseBody)
             ->withHeader('Content-Type', 'application/xml');
 
         return $response;
     }
 
-    public function withRedirect($location): void
+    public function withRedirect($location, $status = 200)
     {
-        die(var_dump($location));
-        header('Location', $location);
+        $response = $this->make($status)
+            ->withHeader('Location', $location);
+        return $response;
+        //header('Location', $location);
     }
 
-    public function withDownload()
+    public function withDownload($filename = '', $pathLocation)
     {
-        // @todo
+        $data = $pathLocation;
+
+        if ($filename == '' || $data == '') {
+            return false;
+        }
+
+        if (!file_exists($data)) {
+            return false;
+        }
+
+        if (false === strpos($filename, '.')) {
+            return false;
+        }
+
+        $extension = strtolower(pathinfo(basename($filename), PATHINFO_EXTENSION));
+
+        $mime_types = array(
+            'txt' => 'text/plain',
+            'htm' => 'text/html',
+            'html' => 'text/html',
+            'php' => 'text/html',
+            'css' => 'text/css',
+            'js' => 'application/javascript',
+            'json' => 'application/json',
+            'xml' => 'application/xml',
+            'swf' => 'application/x-shockwave-flash',
+            'flv' => 'video/x-flv',
+            // images
+            'png' => 'image/png',
+            'jpe' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'jpg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'bmp' => 'image/bmp',
+            'ico' => 'image/vnd.microsoft.icon',
+            'tiff' => 'image/tiff',
+            'tif' => 'image/tiff',
+            'svg' => 'image/svg+xml',
+            'svgz' => 'image/svg+xml',
+            // archives
+            'zip' => 'application/zip',
+            'rar' => 'application/x-rar-compressed',
+            'exe' => 'application/x-msdownload',
+            'msi' => 'application/x-msdownload',
+            'cab' => 'application/vnd.ms-cab-compressed',
+            // audio/video
+            'mp3' => 'audio/mpeg',
+            'qt' => 'video/quicktime',
+            'mov' => 'video/quicktime',
+            // adobe
+            'pdf' => 'application/pdf',
+            'psd' => 'image/vnd.adobe.photoshop',
+            'ai' => 'application/postscript',
+            'eps' => 'application/postscript',
+            'ps' => 'application/postscript',
+            // ms office
+            'doc' => 'application/msword',
+            'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            'rtf' => 'application/rtf',
+            'xls' => 'application/vnd.ms-excel',
+            'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'ppt' => 'application/vnd.ms-powerpoint',
+            'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+            // open office
+            'odt' => 'application/vnd.oasis.opendocument.text',
+            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+        );
+
+        if (!isset($mime_types[$extension])) {
+            $mime = 'application/octet-stream';
+        } else {
+            $mime = ( is_array($mime_types[$extension]) ) ? $mime_types[$extension][0] : $mime_types[$extension];
+        }
+
+        if (strstr($_SERVER['HTTP_USER_AGENT'], "MSIE")) {
+            header('Content-Type: "' . $mime . '"');
+            header('Content-Disposition: attachment; filename="' . $filename . '"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header("Content-Transfer-Encoding: binary");
+            header('Pragma: public');
+            header("Content-Length: " . filesize($data));
+        } else {
+            header("Pragma: public");
+            header("Expires: 0");
+            header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+            header("Cache-Control: private", false);
+            header("Content-Type: " . $mime, true, 200);
+            header('Content-Length: ' . filesize($data));
+            header('Content-Disposition: attachment; filename=' . $filename);
+            header("Content-Transfer-Encoding: binary");
+        }
+        try {
+            readfile(App::getInstance()->getConfig('path_public') . $data);
+        } catch (\Exception $e) {
+            echo $e;
+            throw new \Exception("Path Not Found");
+        }
+        exit;
     }
 }
