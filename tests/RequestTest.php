d72ca6b8c0e823ebf7149e1225f65d0b2f6a2860
fatkulnurk <fatkulnurk@gmail.com>
2020-05-28 17:13:46 +0700

    change to singleton

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -4,19 +4,20 @@ use Fatkulnurk\Microframework\Http\Message\Request;
 use Fatkulnurk\Microframework\Http\Message\Uri;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\StreamInterface;
+use Fatkulnurk\Microframework\Http\Message\Stream;
 
 class RequestTest extends TestCase
 {
     public function testRequestUriMayBeString()
     {
-        $r = new Request('GET', '/');
+        $r = Request::getInstance()->make('GET', '/');
         $this->assertEquals('/', (string) $r->getUri());
     }
 
     public function testRequestUriMayBeUri()
     {
         $uri = new Uri('/');
-        $r = new Request('GET', $uri);
+        $r = Request::getInstance()->make('GET', $uri);
         $this->assertSame($uri, $r->getUri());
     }
 
@@ -25,26 +26,26 @@ class RequestTest extends TestCase
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('Unable to parse URI: ///');
 
-        new Request('GET', '///');
+        Request::getInstance()->make('GET', '///');
     }
 
     public function testCanConstructWithBody()
     {
-        $r = new Request('GET', '/', [], 'baz');
+        $r = Request::getInstance()->make('GET', '/', [], 'baz');
         $this->assertInstanceOf(StreamInterface::class, $r->getBody());
         $this->assertEquals('baz', (string) $r->getBody());
     }
 
     public function testNullBody()
     {
-        $r = new Request('GET', '/', [], null);
+        $r = Request::getInstance()->make('GET', '/', [])->withBody(Stream::create(''));
         $this->assertInstanceOf(StreamInterface::class, $r->getBody());
         $this->assertSame('', (string) $r->getBody());
     }
 
     public function testFalseyBody()
     {
-        $r = new Request('GET', '/', [], '0');
+        $r = Request::getInstance()->make('GET', '/', [], '0');
         $this->assertInstanceOf(StreamInterface::class, $r->getBody());
         $this->assertSame('0', (string) $r->getBody());
     }
@@ -55,13 +56,13 @@ class RequestTest extends TestCase
         $body->expects($this->never())
             ->method('__toString');
 
-        $r = new Request('GET', '/', [], $body);
+        $r = Request::getInstance()->make('GET', '/', [], $body);
         $this->assertSame($body, $r->getBody());
     }
 
     public function testWithUri()
     {
-        $r1 = new Request('GET', '/');
+        $r1 = Request::getInstance()->make('GET', '/');
         $u1 = $r1->getUri();
         $u2 = new Uri('http://www.example.com');
         $r2 = $r1->withUri($u2);
@@ -69,7 +70,7 @@ class RequestTest extends TestCase
         $this->assertSame($u2, $r2->getUri());
         $this->assertSame($u1, $r1->getUri());
 
-        $r3 = new Request('GET', '/');
+        $r3 = Request::getInstance()->make('GET', '/');
         $u3 = $r3->getUri();
         $r4 = $r3->withUri($u3);
         $this->assertSame($r3, $r4, 'If the Request did not change, then there is no need to create a new request object');
@@ -81,14 +82,14 @@ class RequestTest extends TestCase
 
     public function testSameInstanceWhenSameUri()
     {
-        $r1 = new Request('GET', 'http://foo.com');
+        $r1 = Request::getInstance()->make('GET', 'http://foo.com');
         $r2 = $r1->withUri($r1->getUri());
         $this->assertSame($r1, $r2);
     }
 
     public function testWithRequestTarget()
     {
-        $r1 = new Request('GET', '/');
+        $r1 = Request::getInstance()->make('GET', '/');
         $r2 = $r1->withRequestTarget('*');
         $this->assertEquals('*', $r2->getRequestTarget());
         $this->assertEquals('/', $r1->getRequestTarget());
@@ -96,20 +97,20 @@ class RequestTest extends TestCase
 
     public function testWithInvalidRequestTarget()
     {
-        $r = new Request('GET', '/');
+        $r = Request::getInstance()->make('GET', '/');
         $this->expectException(\InvalidArgumentException::class);
         $r->withRequestTarget('foo bar');
     }
 
     public function testGetRequestTarget()
     {
-        $r = new Request('GET', 'https://nyholm.tech');
+        $r = Request::getInstance()->make('GET', 'https://dibumi.com');
         $this->assertEquals('/', $r->getRequestTarget());
 
-        $r = new Request('GET', 'https://nyholm.tech/foo?bar=baz');
+        $r = Request::getInstance()->make('GET', 'https://dibumi.com/foo?bar=baz');
         $this->assertEquals('/foo?bar=baz', $r->getRequestTarget());
 
-        $r = new Request('GET', 'https://nyholm.tech?bar=baz');
+        $r = Request::getInstance()->make('GET', 'https://dibumi.com?bar=baz');
         $this->assertEquals('/?bar=baz', $r->getRequestTarget());
     }
 
@@ -118,35 +119,35 @@ class RequestTest extends TestCase
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('Invalid request target provided; cannot contain whitespace');
 
-        $r1 = new Request('GET', '/');
+        $r1 = Request::getInstance()->make('GET', '/');
         $r1->withRequestTarget('/foo bar');
     }
 
     public function testRequestTargetDefaultsToSlash()
     {
-        $r1 = new Request('GET', '');
+        $r1 = Request::getInstance()->make('GET', '');
         $this->assertEquals('/', $r1->getRequestTarget());
-        $r2 = new Request('GET', '*');
+        $r2 = Request::getInstance()->make('GET', '*');
         $this->assertEquals('*', $r2->getRequestTarget());
-        $r3 = new Request('GET', 'http://foo.com/bar baz/');
+        $r3 = Request::getInstance()->make('GET', 'http://foo.com/bar baz/');
         $this->assertEquals('/bar%20baz/', $r3->getRequestTarget());
     }
 
     public function testBuildsRequestTarget()
     {
-        $r1 = new Request('GET', 'http://foo.com/baz?bar=bam');
+        $r1 = Request::getInstance()->make('GET', 'http://foo.com/baz?bar=bam');
         $this->assertEquals('/baz?bar=bam', $r1->getRequestTarget());
     }
 
     public function testBuildsRequestTargetWithFalseyQuery()
     {
-        $r1 = new Request('GET', 'http://foo.com/baz?0');
+        $r1 = Request::getInstance()->make('GET', 'http://foo.com/baz?0');
         $this->assertEquals('/baz?0', $r1->getRequestTarget());
     }
 
     public function testHostIsAddedFirst()
     {
-        $r = new Request('GET', 'http://foo.com/baz?bar=bam', ['Foo' => 'Bar']);
+        $r = Request::getInstance()->make('GET', 'http://foo.com/baz?bar=bam', ['Foo' => 'Bar']);
         $this->assertEquals([
             'Host' => ['foo.com'],
             'Foo' => ['Bar'],
@@ -155,7 +156,9 @@ class RequestTest extends TestCase
 
     public function testCanGetHeaderAsCsv()
     {
-        $r = new Request('GET', 'http://foo.com/baz?bar=bam', [
+        $r = Request::getInstance()
+            ->flushData()
+            ->make('GET', 'http://foo.com/baz?bar=bam', [
             'Foo' => ['a', 'b', 'c'],
         ]);
         $this->assertEquals('a, b, c', $r->getHeaderLine('Foo'));
@@ -164,7 +167,9 @@ class RequestTest extends TestCase
 
     public function testHostIsNotOverwrittenWhenPreservingHost()
     {
-        $r = new Request('GET', 'http://foo.com/baz?bar=bam', ['Host' => 'a.com']);
+        $r = Request::getInstance()
+            ->flushData()
+            ->make('GET', 'http://foo.com/baz?bar=bam', ['Host' => 'a.com']);
         $this->assertEquals(['Host' => ['a.com']], $r->getHeaders());
         $r2 = $r->withUri(new Uri('http://www.foo.com/bar'), true);
         $this->assertEquals('a.com', $r2->getHeaderLine('Host'));
@@ -172,7 +177,9 @@ class RequestTest extends TestCase
 
     public function testOverridesHostWithUri()
     {
-        $r = new Request('GET', 'http://foo.com/baz?bar=bam');
+        $r = Request::getInstance()
+            ->flushData()
+            ->make('GET', 'http://foo.com/baz?bar=bam');
         $this->assertEquals(['Host' => ['foo.com']], $r->getHeaders());
         $r2 = $r->withUri(new Uri('http://www.baz.com/bar'));
         $this->assertEquals('www.baz.com', $r2->getHeaderLine('Host'));
@@ -180,7 +187,9 @@ class RequestTest extends TestCase
 
     public function testAggregatesHeaders()
     {
-        $r = new Request('GET', '', [
+        $r = Request::getInstance()
+            ->flushData()
+            ->make('GET', '', [
             'ZOO' => 'zoobar',
             'zoo' => ['foobar', 'zoobar'],
         ]);
@@ -190,7 +199,9 @@ class RequestTest extends TestCase
 
     public function testSupportNumericHeaders()
     {
-        $r = new Request('GET', '', [
+        $r = Request::getInstance()
+            ->flushData()
+            ->make('GET', '', [
             'Content-Length' => 200,
         ]);
         $this->assertSame(['Content-Length' => ['200']], $r->getHeaders());
@@ -199,13 +210,13 @@ class RequestTest extends TestCase
 
     public function testAddsPortToHeader()
     {
-        $r = new Request('GET', 'http://foo.com:8124/bar');
+        $r = Request::getInstance()->make('GET', 'http://foo.com:8124/bar');
         $this->assertEquals('foo.com:8124', $r->getHeaderLine('host'));
     }
 
     public function testAddsPortToHeaderAndReplacePreviousPort()
     {
-        $r = new Request('GET', 'http://foo.com:8124/bar');
+        $r = Request::getInstance()->make('GET', 'http://foo.com:8124/bar');
         $r = $r->withUri(new Uri('http://foo.com:8125/bar'));
         $this->assertEquals('foo.com:8125', $r->getHeaderLine('host'));
     }
@@ -214,33 +225,35 @@ class RequestTest extends TestCase
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('Header name must be an RFC 7230 compatible string.');
-        $r = new Request('GET', 'https://example.com/');
+        $r = Request::getInstance()->make('GET', 'https://example.com/');
         $r->withHeader('', 'Bar');
     }
 
     public function testCanHaveHeaderWithEmptyValue()
     {
-        $r = new Request('GET', 'https://example.com/');
+        $r = Request::getInstance()->make('GET', 'https://example.com/');
         $r = $r->withHeader('Foo', '');
         $this->assertEquals([''], $r->getHeader('Foo'));
     }
 
     public function testUpdateHostFromUri()
     {
-        $request = new Request('GET', '/');
+        $request = Request::getInstance()->make('GET', '/');
         $request = $request->withUri(new Uri('https://domain.tld'));
         $this->assertEquals('domain.tld', $request->getHeaderLine('Host'));
 
-        $request = new Request('GET', 'https://example.com/');
+        $request = Request::getInstance()
+            ->flushData()
+            ->make('GET', new Uri('https://example.com/'));
         $this->assertEquals('example.com', $request->getHeaderLine('Host'));
         $request = $request->withUri(new Uri('https://domain.tld'));
         $this->assertEquals('domain.tld', $request->getHeaderLine('Host'));
 
-        $request = new Request('GET', '/');
+        $request = Request::getInstance()->make('GET', '/');
         $request = $request->withUri(new Uri('https://domain.tld:8080'));
         $this->assertEquals('domain.tld:8080', $request->getHeaderLine('Host'));
 
-        $request = new Request('GET', '/');
+        $request = Request::getInstance()->make('GET', '/');
         $request = $request->withUri(new Uri('https://domain.tld:443'));
         $this->assertEquals('domain.tld', $request->getHeaderLine('Host'));
     }
